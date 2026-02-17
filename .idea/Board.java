16668193cfa2c667f7f3de19902b094ad86ee826
AY4LIFE <168647944+AY4LIFE@users.noreply.github.com>
2026-02-17 11:47:59 -0700

    Implemented movePiece to validate and allow movement of pieces

--- .idea/Board.java
+++ .idea/Board.java
@@ -1,9 +1,9 @@
 public class Board {
     private int[][] board;
     private final int SIZE = 8;
-    private final int EMPTY = 0;
-    private final int BLACK = 1;
-    private final int RED = 2;
+    public static int EMPTY = 0;
+    public static final int BLACK = 1;
+    public static final int RED = 2;
 
     public Board(){
         board = new int[SIZE][SIZE];
@@ -23,7 +23,7 @@ public class Board {
         }
     }
     public void printBoard(){
-        System.out.print(" ");
+        System.out.print("   ");
         for (int col = 0; col < SIZE; col++){
             System.out.print(col + " ");
         }
@@ -39,6 +39,39 @@ public class Board {
             }
             System.out.println();
         }
-
     }
+    public boolean movePiece(int startRow, int startCol, int endRow,
+                             int endCol, int currentPlayer){
+
+        if (startRow < 0 || startRow >= SIZE ||
+                startCol < 0 || startCol >= SIZE ||
+                endRow < 0 || endRow >= SIZE ||
+                endCol < 0 || endCol >= SIZE) {
+            return false;
+
+    }else if (board[startRow][startCol] == EMPTY) return false;
+        else if (board[startRow][startCol] != currentPlayer) return false;
+        else if (board[endRow][endCol] != EMPTY) return false;
+
+        else{
+            if (currentPlayer == BLACK){
+                if ((endRow == startRow + 1) && (Math.abs(endCol - startCol) == 1)){
+                    board[endRow][endCol] = board[startRow][startCol];
+                    board[startRow][startCol] = EMPTY;
+                    return true;
+                }
+                else return false;
+            }
+            else if (currentPlayer == RED){
+                if ((endRow == startRow - 1) && (Math.abs(endCol - startCol) == 1)){
+                    board[endRow][endCol] = board[startRow][startCol];
+                    board[startRow][startCol] = EMPTY;
+                    return true;
+                }
+                else return false;
+            }
+        }
+        return false;
+    }
+
 }
